ab7e59fcdaeaea4e42e8bbbb20a485048696972a
Abrar Fahyaz <140268369+abrr-fhyz@users.noreply.github.com>
2024-03-29 21:04:32 +0600

    Update Database.java

--- CSE2102_StudentDatabase/Database.java
+++ CSE2102_StudentDatabase/Database.java
@@ -2,9 +2,10 @@ import java.util.HashMap;
 
 public class Database{
 	public static void main(String [] args){
-		boolean dummy;
 		HashMap<Integer, Student> list_of_students = new HashMap<>();
-		Student student = new Student("Abrar", 1, "abrar@gmail.com");
+		//custom students
+		boolean dummy;
+		Student student = new Student("Abrar Fahyaz", 1, "abrar@gmail.com");
 		student.addCourse("EmbeddedSystems", 70, 25, 5);
 		student.addCourse("ArtificialIntelligence", 80, 15, 5);
 		student.addCourse("Security", 60, 30, 10);
@@ -14,7 +15,7 @@ public class Database{
 		dummy = student.addCourseGrade("Security", 43, 12, 10);
 		dummy = student.addCourseGrade("Networking", 51, 21, 7);
 		list_of_students.put(1, student);
-		student = new Student("Obama", 2, "brac@obamna.usa");
+		student = new Student("Barack H. Obama", 2, "brac@obamna.usa");
 		student.addCourse("EmbeddedSystems", 70, 25, 5);
 		student.addCourse("ArtificialIntelligence", 80, 15, 5);
 		student.addCourse("OperationResearch", 60, 30, 10);
@@ -24,16 +25,17 @@ public class Database{
 		dummy = student.addCourseGrade("Networking", 63, 18, 9);
 		dummy = student.addCourseGrade("OperationResearch", 55, 20, 9);
 		list_of_students.put(2, student);
-		student = new Student("Fahyaz", 3, "fahyaz@gmail.com");
+		student = new Student("Maisha Rahman", 3, "mmrrhm@gmail.com");
 		student.addCourse("EmbeddedSystems", 70, 25, 5);
 		student.addCourse("ArtificialIntelligence", 80, 15, 5);
 		student.addCourse("Security", 60, 30, 10);
 		student.addCourse("OperationResearch", 60, 30, 10);
-		dummy = student.addCourseGrade("EmbeddedSystems", 51, 15, 3);
-		dummy = student.addCourseGrade("ArtificialIntelligence", 60, 15, 4);
-		dummy = student.addCourseGrade("Security", 45, 22, 5);
-		dummy = student.addCourseGrade("OperationResearch", 55, 20, 9);
+		dummy = student.addCourseGrade("EmbeddedSystems", 70, 25, 3);
+		dummy = student.addCourseGrade("ArtificialIntelligence", 75, 15, 4);
+		dummy = student.addCourseGrade("Security", 55, 28, 9);
+		dummy = student.addCourseGrade("OperationResearch", 60, 30, 9);
 		list_of_students.put(3, student);
+		Menu.createRandomStudents(list_of_students, 20);
 		Menu.mainMenu(list_of_students);
 	}
 }
