0f4c018518efe7af379360470887edc490b06698
Abrar Fahyaz <140268369+abrr-fhyz@users.noreply.github.com>
2024-05-20 00:34:58 +0600

    Delete OOP_Project_ShareTune_Frontend directory

--- OOP_Project_ShareTune_Frontend/build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-javac --module-path "C:\Program Files (x86)\Java\javafx-sdk-22\lib" --add-modules javafx.controls,javafx.fxml,javafx.media src/*.java
-cd src
-java --module-path "C:\Program Files (x86)\Java\javafx-sdk-22\lib" --add-modules javafx.controls,javafx.fxml,javafx.media Main
